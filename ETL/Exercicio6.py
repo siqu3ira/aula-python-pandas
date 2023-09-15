652d9dad2415568df7013cfd362d9b6e2b964b2f
Maurilio Siqueira <mauriliosiqueira2001@hotmail.com>
2023-09-15 12:20:08 -0300

    Aula 6 ETL finalizada

---
+++ ETL/Exercicio6.py
@@ -0,0 +1,30 @@
+import pandas as pd 
+import numpy as np
+import datetime as dt
+
+df = pd.read_csv('https://www.harve.com.br/praticas/dados_etl.csv', sep="\t") # separando quando o delimitador é uma \
+
+df = df.rename(columns={'Client ID': 'CLIENT_ID'})
+
+def recebimento(valor):
+  if valor<10000:
+    return 'classe d'
+  else:
+    if valor < 30000:
+      return 'classe c'
+    else:
+      if valor < 60000:
+        return 'classe b'
+      else:
+        if valor >0:
+          return 'classe a'
+        else:
+          return ''
+
+classe = df['Income']
+print(classe)
+print()
+
+df['Classe'] = classe.apply (lambda x: recebimento(x))
+print(df['Classe'].value_counts())
+
